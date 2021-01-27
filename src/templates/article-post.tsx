4a11fb57be749bec328f98d702c918f430c5c750
mathiascs <mathias@codeshape.io>
2021-01-27 23:35:25 +0100

    Update article-post.tsx

--- src/templates/article-post.tsx
+++ src/templates/article-post.tsx
@@ -2,8 +2,14 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import tw from "tailwind-styled-components"
+import styled from "styled-components"
 import Layout from "../components/layout/layout"
+import {
+  BodyMain,
+  Caption,
+  H1,
+  MediumText,
+} from "../components/styles/TextStyles"
 export default function BlogPostTemplate({
   data: { authorImage, coverImage },
   pageContext: { nextPost, page, previousPost },
@@ -12,69 +18,42 @@ export default function BlogPostTemplate({
     <Layout>
       <Wrapper>
         <Header>
-          <div className="space-y-1 text-center">
-            <dl className="space-y-10">
-              <div>
-                <dt className="sr-only">Published on</dt>
-                <dd className="text-base leading-6 font-medium text-gray-500">
-                  <time dateTime={page.date}>{page.date}</time>
-                </dd>
-              </div>
-            </dl>
-            <PageTitle>{page.title}</PageTitle>
-          </div>
+          <PublishDate>{page.date}</PublishDate>
+          <PageTitle>{page.title}</PageTitle>
         </Header>
-        <div
-          className="divide-y lg:divide-y-0 divide-gray-200 lg:grid lg:grid-cols-4 lg:col-gap-6 pb-16 lg:pb-20"
-          style={{ gridTemplateRows: "auto 1fr" }}
-        >
-          <dl className="pt-6 pb-10 lg:pt-11 lg:border-b lg:border-gray-200">
-            <dt className="sr-only">Author</dt>
-            <dd>
-              <ul className="flex justify-center lg:block space-x-8 sm:space-x-12 lg:space-x-0 lg:space-y-8">
-                <li className="flex space-x-2">
-                  <Img
-                    fluid={authorImage.localFile.childImageSharp.fluid}
-                    className="w-10 h-10 rounded-full"
-                    fadeIn={false}
-                  />
-                  <dl className="flex-1 text-sm font-medium leading-5">
-                    <dt className="sr-only">Name</dt>
-                    <dd className="text-900">{page.author.name}</dd>
-                    {page.author.title && (
-                      <React.Fragment>
-                        <dt className="sr-only">Title</dt>
-                        <dd className="text-gray-500">{page.author.title}</dd>
-                      </React.Fragment>
-                    )}
-                  </dl>
-                </li>
-              </ul>
-            </dd>
-          </dl>
-          <Article>
-            {coverImage && (
+        <ArticleWrapper>
+          <AuthorWrapper>
+            <AuthorAvatar>
               <Img
-                fluid={coverImage.localFile.childImageSharp.fluid}
-                className="mb-8 rounded"
+                fluid={authorImage.localFile.childImageSharp.fluid}
+                className="AuthorImage"
                 fadeIn={false}
               />
-            )}
-            <Content>
+            </AuthorAvatar>
+            <TextWrapper>
+              <AuthorName>{page.author.name}</AuthorName>
+              <AuthorTitle>{page.author.title}</AuthorTitle>
+            </TextWrapper>
+          </AuthorWrapper>
+          <Article>
+            <ContentWrapper>
+              <Img
+                fluid={coverImage.localFile.childImageSharp.fluid}
+                className="CoverImage"
+                fadeIn={false}
+              />
               <MDXRenderer>
                 {page.content.markdownNode.childMdx.body}
               </MDXRenderer>
-            </Content>
+            </ContentWrapper>
           </Article>
           <Footer>
             {(nextPost || previousPost) && (
-              <div className="space-y-8 py-8">
+              <div>
                 {nextPost && (
-                  <div>
-                    <h2 className="text-xs tracking-wide uppercase text-gray-500">
-                      Next Post
-                    </h2>
-                    <div className="text-purple-500 hover:text-purple-600">
+                  <div className="Navigation">
+                    <h2>Next Post</h2>
+                    <div>
                       <Link to={`/articles/${nextPost.slug}`}>
                         {nextPost.title}
                       </Link>
@@ -82,11 +61,9 @@ export default function BlogPostTemplate({
                   </div>
                 )}
                 {previousPost && (
-                  <div>
-                    <h2 className="text-xs tracking-wide uppercase text-gray-500">
-                      Previous Post
-                    </h2>
-                    <div className="text-purple-500 hover:text-purple-600">
+                  <div className="Navigation">
+                    <h2>Previous Post</h2>
+                    <div>
                       <Link to={`/articles/${previousPost.slug}`}>
                         {previousPost.title}
                       </Link>
@@ -95,16 +72,13 @@ export default function BlogPostTemplate({
                 )}
               </div>
             )}
-            <div className="pt-8">
-              <Link
-                to="/articles/"
-                className="text-purple-500 hover:text-purple-600"
-              >
+            <div>
+              <Link to="/articles/" className="">
                 &larr; Back to the blog
               </Link>
             </div>
           </Footer>
-        </div>
+        </ArticleWrapper>
       </Wrapper>
     </Layout>
   )
@@ -137,44 +111,105 @@ export const pageQuery = graphql`
   }
 `
 
-const Wrapper = tw.div`
-max-w-5xl
-mx-auto
-`
-const Header = tw.div`
-pt-6 
-lg:pb-10
-`
-const PageTitle = tw.div`
-text-3xl 
-leading-9 
-font-extrabold 
-tracking-tight 
-sm:text-4xl 
-sm:leading-10 
-md:text-5xl 
-md:leading-14
-`
-const Article = tw.div`
-divide-y 
-divide-gray-200 
-lg:pb-0 
-lg:col-span-3 
-lg:row-span-2
-`
-const Content = tw.div`
-prose 
-max-w-none 
-pt-10 
-pb-8
+const Wrapper = styled.div``
+
+const ArticleWrapper = styled.div`
+  margin: 1.875rem;
+  display: grid;
+  grid-gap: 1.875rem;
+  grid-template-columns: auto auto;
 `
 
-const Footer = tw.div`
-text-sm 
-font-medium 
-leading-5 
-divide-y 
-divide-gray-200 
-lg:col-start-1 
-lg:row-start-2
+const Article = styled.div``
+
+const Header = styled.div`
+  text-align: center;
+  margin: 0 auto;
+`
+
+const PublishDate = styled(BodyMain)`
+  color: #757372;
+`
+
+const PageTitle = styled(H1)``
+
+const AuthorWrapper = styled.div`
+  .Author {
+    grid-area: Author;
+  }
+  max-width: 20rem;
+  max-height: 4rem;
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: auto auto;
+  align-content: center;
+  grid-gap: 1.25rem;
+  border-radius: 1.25rem;
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+
+  @supports (backdrop-filter: blur(40px)) or
+    (-webkit-backdrop-filter: blur(40px)) {
+    opacity: 0, 5;
+    background-color: transparent;
+    -webkit-backdrop-filter: blur(40px);
+    backdrop-filter: blur(40px);
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+
+  @-moz-document url-prefix() {
+    background-color: rgba(242, 242, 242, 0.7);
+    @media (prefers-color-scheme: dark) {
+      background-color: rgba(50, 50, 52, 0.7);
+    }
+  }
+`
+
+const AuthorAvatar = styled.div`
+  .AuthorImage {
+    border: 3px solid white;
+    border-radius: 50%;
+    width: 4rem;
+    height: 4rem;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    justify-self: center;
+    position: relative;
+  }
+`
+
+const TextWrapper = styled.div`
+  display: grid;
+  min-height: 1.25rem;
+`
+
+const AuthorName = styled(MediumText)`
+  font-weight: 900;
+  color: black;
+  @media (prefers-color-scheme: dark) {
+    color: white;
+  }
+`
+
+const AuthorTitle = styled(Caption)`
+  color: #757372;
+`
+
+const ContentWrapper = styled.div`
+  display: grid;
+  gap: 1.25rem;
+
+  .CoverImage {
+    border-radius: 1.25rem;
+  }
+`
+
+const Footer = styled.div`
+  display: grid;
+  grid-gap: 1.25rem;
+
+  .Navigation {
+    display: grid;
+    grid-gap: 1.25rem;
+  }
 `
