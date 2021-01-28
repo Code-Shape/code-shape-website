4c8b3e97465fca78a957629f089be1e3a455a44e
Mathias Riis Sørensen <72079659+mathiascs@users.noreply.github.com>
2021-01-28 16:09:30 +0100

    Adding info in gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
-import React from "react"
-import { MDXProvider } from "@mdx-js/react"
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
 
+/* eslint-disable */
 const components = {
+  wrapper: ({children}) => <>{children}</>,
   h1: props => (
     <h1 style={{ fontSize: "60px", fontWeight: "bold" }} {...props} />
   ),
@@ -19,7 +22,34 @@ const components = {
   ),
   strong: props => (
     <strong style={{ fontWeight: "900", lineHeight: 1.6 }} {...props} />
-  )
+  ),
+  pre: (props) => {
+    const className = props.children.props.className || '';
+    const matches = className.match(/language-(?<lang>.*)/);
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children}
+        language={
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ''
+        }
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    );
+  },
 }
 
 export const wrapRootElement = ({ element }) => (
