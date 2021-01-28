17216dcd65a59b7419777214316abd70c05bddb8
Mathias Riis Sørensen <72079659+mathiascs@users.noreply.github.com>
2021-01-28 11:48:54 +0100

    optimization in browser, lock and package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,36 +1,25 @@
-import "./src/components/layout/layout.css"
 import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 
-const H1 = props => (
-  <h1 style={{ fontSize: "60px", fontWeight: "bold" }} {...props} />
-)
-
-const H2 = props => (
-  <h2 style={{ fontSize: "40px", fontWeight: "bold" }} {...props} />
-)
-
-const H3 = props => (
-  <h2 style={{ fontSize: "30px", fontWeight: "bold" }} {...props} />
-)
-
-const H4 = props => (
-  <h4 style={{ fontSize: "20px", fontWeight: "bold" }} {...props} />
-)
-
-const MyParagraph = props => (
-  <p style={{ fontSize: "16px", lineHeight: 1.6 }} {...props} />
-)
-
-const Strong = props => <strong style={{ fontWeight: "bold" }} {...props} />
-
 const components = {
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  h4: H4,
-  p: MyParagraph,
-  strong: Strong,
+  h1: props => (
+    <h1 style={{ fontSize: "60px", fontWeight: "bold" }} {...props} />
+  ),
+  h2: props => (
+    <h2 style={{ fontSize: "40px", fontWeight: "bold" }} {...props} />
+  ),
+  h3: props => (
+    <h3 style={{ fontSize: "30px", fontWeight: "bold" }} {...props} />
+  ),
+  h4: props => (
+    <h4 style={{ fontSize: "20px", fontWeight: "bold" }} {...props} />
+  ),  
+  p: props => (
+    <p style={{ fontSize: "16px", lineHeight: 1.6 }} {...props} />
+  ),
+  strong: props => (
+    <strong style={{ fontWeight: "900", lineHeight: 1.6 }} {...props} />
+  )
 }
 
 export const wrapRootElement = ({ element }) => (
