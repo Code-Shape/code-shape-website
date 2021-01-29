c048deec808abc6ba7850fd197c8588253cbdba6
Mathias Riis Sørensen <72079659+mathiascs@users.noreply.github.com>
2021-01-29 14:50:33 +0100

    Update default-page.tsx

--- src/templates/default-page.tsx
+++ src/templates/default-page.tsx
@@ -1,31 +1,57 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import styled from "styled-components"
+import { H1, BodyMain } from "../components/styles/TextStyles"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
 export default function DefaultPageTemplate({ pageContext: { page } }) {
   return (
     <Layout>
       <Wrapper>
+        <HeaderWrapper>
+          <TextWrapper>
+            <TitleWrapper>{page.title}</TitleWrapper>
+            <SubtitleWrapper>{page.subtitle}</SubtitleWrapper>
+          </TextWrapper>
+        </HeaderWrapper>
         <ContentWrapper>
-          <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {page.title}
-          </h1>
-          {page.subtitle && (
-            <p className="text-lg leading-7 text-gray-500">{page.subtitle}</p>
-          )}
+          <MDXRenderer>{page.content.markdownNode.childMdx.body}</MDXRenderer>
         </ContentWrapper>
-        <div className="pb-16 lg:pb-20">
-          <div className="prose max-w-none pt-10 pb-8">
-            <MDXRenderer>{page.content.markdownNode.childMdx.body}</MDXRenderer>
-          </div>
-        </div>
       </Wrapper>
     </Layout>
   )
 }
 const Wrapper = styled.div`
-  margin: 0 auto;
 `
 
-const ContentWrapper = styled.div``
+const HeaderWrapper = styled.div`
+  min-height: 250px;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`
+
+const TextWrapper = styled.div`
+  display: grid;
+  grid-gap: 1rem;  
+  text-align: center;
+
+`
+
+const TitleWrapper = styled(H1)`
+font-weight: 900;
+background: -webkit-linear-gradient(left, #7230ce, #3E16BB)};
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;`
+
+const SubtitleWrapper = styled(BodyMain)`
+`
+
+const ContentWrapper = styled.div`
+  max-width: 70rem;
+  margin: 0 auto;
+  padding: 1rem;
+  display: grid;
+  grid-gap: 1rem;
+`
