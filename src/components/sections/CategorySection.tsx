14fa725d26d62cf052d5f806b8928f3a76fa771e
mathiascs <mathias@codeshape.io>
2021-02-07 13:17:43 +0100

    Update CategorySection.tsx
    CSS adjustments

--- src/components/sections/CategorySection.tsx
+++ src/components/sections/CategorySection.tsx
@@ -53,8 +53,7 @@ const ContentWrapper = styled.div`
 `
 
 const TextWrapper = styled.div`
-  display: grid;
-  grid-gap: 0.5rem;
+  line-height: 3rem;
 `
 
 const Title = styled(H3)``
@@ -63,14 +62,40 @@ const Description = styled(MediumText)``
 
 const CategoryWrapper = styled.div`
   display: grid;
-  grid-gap: 0.2rem;
-  grid-template-columns: auto auto auto auto;
+  grid-gap: 0.5rem;
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
 `
 
 const Categories = styled.div`
-  color: white;
-  background-color: #ffa100;
   border-radius: 0.8rem;
   text-align: center;
   padding: 0.4rem;
+  margin: 0 auto;
+
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+
+  @supports (backdrop-filter: blur(40px)) or
+    (-webkit-backdrop-filter: blur(40px)) {
+    opacity: 0, 5;
+    background-color: transparent;
+    -webkit-backdrop-filter: blur(40px);
+    backdrop-filter: blur(40px);
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+
+  @-moz-document url-prefix() {
+    background-color: rgba(242, 242, 242, 0.7);
+    @media (prefers-color-scheme: dark) {
+      background-color: rgba(50, 50, 52, 0.7);
+    }
+  }
+
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  :hover {
+    transform: translateY(-3px);
+  }
 `
