b5bf7888971a25034bfcebb60c20031ca26850f5
smr11 <sune_mikkel@hotmail.com>
2021-03-10 16:43:13 +0100

    Updated to Gatsby v. 3 and prepared Firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
-require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 const path = require("path")
 
@@ -83,6 +85,24 @@ module.exports = {
       options: {
         plugins: [`gatsby-remark-reading-time`],
       },
+    },
+    // {
+    //  resolve: `gatsby-plugin-create-client-paths`,
+    //  options: { prefixes: [`/app/*`] },
+    // },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.GATSBY_FIREBASE_APP_ID,
+        }
+      }
     },
     // {
     //   // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
